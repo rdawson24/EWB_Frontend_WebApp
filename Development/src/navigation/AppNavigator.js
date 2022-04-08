1009fbffbb760e7c328c4407985623d6c9e229d3
JayWilling <jwilling123@gmail.com>
2022-04-08 15:19:50 +1000

    Basic navigation complete

--- Development/src/navigation/AppNavigator.js
+++ Development/src/navigation/AppNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { creativeNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import DashboardScreen from '../screens/DashboardScreen';
 import BookingsScreen from '../screens/BookingsScreen';
@@ -9,97 +9,53 @@ import LoginScreen from '../screens/LoginScreen';
 import ReportScreen from '../screens/ReportsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import UsersScreen from '../screens/UsersScreen';
+import { DrawerSidebar } from 'react-navigation-drawer';
 
-
-// Following this documentation, rather than the udemy code:
-// https://reactnavigation.org/docs/hello-react-navigation
-// https://reactnavigation.org/docs/tab-based-navigation/
-
-function DashboardScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Dashboard screen</Text>
-        </View>
-    );
-}
-
-function BookingsScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Bookings screen</Text>
-        </View>
-    );
-}
-
-function IncomeScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Income screen</Text>
-        </View>
-    );
-}
-
-function ReportScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Reports screen</Text>
-        </View>
-    );
-}
-
-function SettingsScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Settings screen</Text>
-        </View>
-    );
-}
-
-function UsersScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Users screen</Text>
-        </View>
-    );
-}
-
-const Stack = creativeNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function AppNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
+            <Drawer.Navigator
+                useLegacyImplementation
+                defaultStatus='open'
+                initialRouteName='Dashboard'
+                goBack='none'
+                screenOptions={
+                    {drawerType: 'permanent', overlayColor: null}
+                }
+            >
+                <Drawer.Screen
                     name="Dashboard"
                     component={DashboardScreen}
                     options={{title: 'Dashboard'}}
                 />
-                <Stack.Screen
+                <Drawer.Screen
                     name="Bookings"
                     component={BookingsScreen}
                     options={{title: 'Bookings'}}
                 />
-                <Stack.Screen
+                <Drawer.Screen
                     name="Income"
                     component={IncomeScreen}
                     options={{title: 'Income'}}
                 />
-                <Stack.Screen
+                <Drawer.Screen
                     name="Reports"
                     component={ReportScreen}
                     options={{title: 'Reports'}}
                 />
-                <Stack.Screen
+                <Drawer.Screen
                     name="Users"
                     component={UsersScreen}
                     options={{title: 'Users'}}
                 />
-                <Stack.Screen
+                <Drawer.Screen
                     name="Settings"
                     component={SettingsScreen}
                     options={{title: 'Settings'}}
                 />
-            </Stack.Navigator>
+            </Drawer.Navigator>
         </NavigationContainer>
     );
 }
